32d7e9c0266447b3cc7e5a9459ed664e442ac0d7
Chong Guo <armourcy@gmail.com>
2023-06-11 00:46:38 +0700

    initial commit

---
+++ laravel/app/Services/PostService.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Services;
+
+use Carbon\Carbon;
+
+class PostService
+{
+    // 
+}
---
+++ laravel/database/migrations/2023_06_10_165926_create_post_images_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_images', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('post_id')->nullable(false);
+            $table->longText('image');
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('posts')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_images');
+    }
+};
